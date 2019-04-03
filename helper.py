b2d88a297f927a92e68d9bd03c434f1be2b76810
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-04-03 15:50:36 +0430

    using "in" for make sure the whole word is ok, just working with file2

--- helper.py
+++ helper.py
@@ -3,25 +3,33 @@ import re
 file1 = "list1.txt";
 file2 = "list2.csv";
 
+# ازبررگه
 textfile = open(file2, 'r')
 text = textfile.read()
 textfile.close()
+
 chars = input("pls enter your chars\n")
+tool = int(input("enter tool : "))
+
 chars.replace(" ","") # removing white spaces
-regex = "[" + str(chars) + "]{2,"+ str(len(chars)) +"}"
+regex = "[" + chars + "]{2,}"
+
 regex = re.compile(regex)
 matches = re.findall(regex, text)
-clean_matches = sorted(set(matches))
+uniq_matches = sorted(set(matches))
 
 ans = ""
-for w in clean_matches :
+for w in uniq_matches :
+    if len(w) != tool: continue;
+    if (","+w+"\n") not in text : continue;
     ans = ans + w + "\n"
-print(ans)
+
+print(" working complete, saving to file..")
 
 out_file = 'ans.txt'
 out = open(out_file,'w')
 out.write(ans)
 out.close()
 
-if len(matches) == 0 : print("no")
-else : print("yeS")
+if ans == "" : print("nothing found")
+else : print("wow, congradualtions")
