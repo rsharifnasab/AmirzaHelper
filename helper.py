3c6bb776fc5f946cf2c4618e7e27796464c4bcb0
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-04-03 16:23:37 +0430

    removed tool checking

--- helper.py
+++ helper.py
@@ -6,32 +6,30 @@ def comp(a,b):
             if a.count(c) != b.count(d) : return False;
     return True;
 
-#file1 = "list1.txt";
 file = "list3.txt";
-
 textfile = open(file, 'r')
 text = textfile.read()
 textfile.close()
 
 chars = input("pls enter your chars\n")
-tool = int(input("enter tool : "))
+tool = -1  #TODO
+#tool = int(input("enter tool : "))
 
 chars.replace(" ","") # removing white spaces
 
 regex = "[" + chars + "]{2,}"
-
 regex = re.compile(regex)
 matches = re.findall(regex, text)
 uniq_matches = sorted(set(matches))
 
 ans = ""
 for w in uniq_matches :
-    if len(w) != tool: continue;
+    if (tool != -1)  and (len(w) != tool): continue;
     if (","+w+"\n") not in text : continue;
     if comp(w,chars) == False : continue;
     ans = ans + w + "\n"
 
-print(" working complete, saving to file..")
+print(" working complete, saving to file . . ")
 
 out_file = 'ans.txt'
 out = open(out_file,'w')
