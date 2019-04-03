c189972242d0e5e8065f6c266745590aa5e0bd8b
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-04-03 14:34:30 +0430

    added git ignore, ignoring ans.txt converting chars to regex and searching but not clean

--- helper.py
+++ helper.py
@@ -3,13 +3,25 @@ import re
 file1 = "list1.txt";
 file2 = "list2.csv";
 
-textfile = open(file1, 'r')
+textfile = open(file2, 'r')
 text = textfile.read()
 textfile.close()
 chars = input("pls enter your chars\n")
 chars.replace(" ","") # removing white spaces
-regex = chars
+regex = "[" + str(chars) + "]{2,}"
+regex = re.compile(regex)
 matches = re.findall(regex, text)
-print("mathc is : \n" ,matches)
+clean_matches = sorted(set(matches))
+
+ans = ""
+for w in clean_matches :
+    ans = ans + w + "\n"
+print(ans)
+
+out_file = 'ans.txt'
+out = open(out_file,'w')
+out.write(ans)
+out.close()
+
 if len(matches) == 0 : print("no")
 else : print("yeS")
